fa22c136098992774b3346cbe40090eba8afec50
Sam Lu <pbcmarsh@gmail.com>
2024-04-26 00:54:00 -0500

    Create TwoNumberSum-1.py

---
+++ TwoNumberSum-1.py
@@ -0,0 +1,10 @@
+# Two Number Sum (Python)
+# https://www.algoexpert.io/questions/two-number-sum
+# Time Complexity: O(n^2) n is length of array
+# Space Complexity: O(1)
+def twoNumberSum(array, targetSum):
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i] + array[j] == targetSum:
+                return [array[i], array[j]]
+    return []
