ddf81613168b1af0a6e1e79b17c3e70079f6ea5b
Sam Lu <pbcmarsh@gmail.com>
2024-04-26 01:52:53 -0500

    Create SortedSquaredArray-2.py

---
+++ SortedSquaredArray-2.py
@@ -0,0 +1,10 @@
+# Sorted Squared Array (Python)
+# https://www.algoexpert.io/questions/sorted-squared-array
+# Time Complexity: O(nlogn) n is length of array
+# Space Complexity: O(n)
+def sortedSquaredArray(array):
+    arr = []
+    for num in array:
+        arr.append(num * num)
+    arr.sort()
+    return arr
