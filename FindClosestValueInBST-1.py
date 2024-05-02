a75d9858f57b1a058100a7d3332698724ffe6c93
Sam Lu <pbcmarsh@gmail.com>
2024-05-02 17:55:52 -0500

    Create FindClosestValueInBST-1.py

---
+++ FindClosestValueInBST-1.py
@@ -0,0 +1,28 @@
+# Find Closest Value in BST (Python)
+# https://www.algoexpert.io/questions/find-closest-value-in-bst
+# Average Time Complexity: O(log(n))
+# Worst Time Complexity: O(n)
+# Average Space Complexity: O(log(n))
+# WOrst Space Complexity: O(n)
+
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, tree.value)
+
+def findClosestValueInBstHelper(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(target - tree.value) < abs(target - closest):
+        closest = tree.value
+    if target > tree.value:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    elif target < tree.value:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    else:
+        return closest
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
