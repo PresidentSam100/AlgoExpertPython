8b535454d5f2075f004697ef031cc2c20caf7c16
Sam Lu <pbcmarsh@gmail.com>
2024-05-02 17:54:45 -0500

    Update SortedSquaredArray-1.py

--- SortedSquaredArray-1.py
+++ SortedSquaredArray-1.py
@@ -1,6 +1,6 @@
 # Sorted Squared Array (Python)
 # https://www.algoexpert.io/questions/sorted-squared-array
-# Time Complexity: O(nlogn) n is length of array
+# Time Complexity: O(n * log(n)) n is length of array
 # Space Complexity: O(n)
 def sortedSquaredArray(array):
     return sorted(list(map(lambda num: num ** 2, array)))
