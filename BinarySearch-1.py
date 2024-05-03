7d7ecd21c6f6eebf662942e871acb4514832caca
Sam Lu <pbcmarsh@gmail.com>
2024-05-03 16:35:59 -0500

    Update BinarySearch-1.py

--- BinarySearch-1.py
+++ BinarySearch-1.py
@@ -1,17 +1,18 @@
 # Binary Search (Python)
 # https://www.algoexpert.io/questions/binary-search
 # Time Complexity: O(log(n)) n is the length of array
-# Space Complexity: O(1)
+# Space Complexity: O(log(n))
 
 def binarySearch(array, target):
-    lo = 0
-    hi = len(array) - 1
-    while lo <= hi:
-        mid = (lo + hi) // 2
-        if array[mid] < target:
-            lo = mid + 1
-        elif array[mid] > target:
-            hi = mid - 1
-        else:
-            return mid
-    return -1
+    return binarySearchHelper(array, 0, len(array) - 1, target)
+
+def binarySearchHelper(array, lo, hi, target):
+    mid = (lo + hi) // 2
+    if lo > hi:
+        return -1
+    elif array[mid] < target:
+        return binarySearchHelper(array, mid + 1, hi, target)
+    elif array[mid] > target:
+        return binarySearchHelper(array, lo, mid - 1, target)
+    else:
+        return mid
