2c6ad9449fa2c02007e353b09a2156b9e47b1e69
Sam Lu <pbcmarsh@gmail.com>
2024-05-03 01:10:32 -0500

    Create InsertionSort-1.py

---
+++ InsertionSort-1.py
@@ -0,0 +1,18 @@
+# Insertion Sort (Python)
+# https://www.algoexpert.io/questions/insertion-sort
+# Best Time Complexity: O(n) n is length of array
+# Average Time Complexity: O(n^2)
+# Worst Time Complexity: O(n^2)
+# Best Space Complexity: O(1)
+# Average Space Complexity: O(1)
+# Worst Space Complexity: O(1)
+
+def insertionSort(array):
+    for i in range(len(array) - 1):
+        j = i + 1
+        temp = array[j]
+        while j > 0 and temp < array[j - 1]:
+            array[j] = array[j - 1]
+            j -= 1
+        array[j] = temp
+    return array
