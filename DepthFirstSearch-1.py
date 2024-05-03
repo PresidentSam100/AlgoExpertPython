c12593e358366130bcc7390129aa14899bcc3325
Sam Lu <pbcmarsh@gmail.com>
2024-05-02 19:11:30 -0500

    Create DepthFirstSearch-1.py

---
+++ DepthFirstSearch-1.py
@@ -0,0 +1,25 @@
+# Depth-first Search (Python)
+# https://www.algoexpert.io/questions/depth-first-search
+# Time Complexity: O(v + e) v is the number of vertices, e is the number of edges
+# Space Complexity: O(v)
+
+
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+        
