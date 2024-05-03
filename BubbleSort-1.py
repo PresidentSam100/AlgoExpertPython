63191ab484199db96463bdbf81efc45b63f60079
Sam Lu <pbcmarsh@gmail.com>
2024-05-02 23:55:50 -0500

    Create BubbleSort-1.py

---
+++ BubbleSort-1.py
@@ -0,0 +1,15 @@
+# Bubble Sort (Python)
+# https://www.algoexpert.io/questions/bubble-sort
+# Best Time Complexity: O(n^2) n is length of array
+# Average Time Complexity: O(n^2)
+# Worst Time Complexity: O(n^2)
+# Best Space Complexity: O(1)
+# Average Space Complexity: O(1)
+# Worst Space Complexity: O(1)
+
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(len(array) - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
