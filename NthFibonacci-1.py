1cd23345beb9f038d0b51bea505bd1520b31352c
Sam Lu <pbcmarsh@gmail.com>
2024-05-04 03:55:48 -0500

    Update NthFibonacci-1.py

--- NthFibonacci-1.py
+++ NthFibonacci-1.py
@@ -1,14 +1,11 @@
 # Nth Fibonacci (Python)
 # https://www.algoexpert.io/questions/nth-fibonacci
-# Time Complexity: O(n) n is the input
-# Space Complexity: O(1)
+# Time Complexity: O(2^n) n is the input number
+# Space Complexity: O(n)
 
 def getNthFib(n):
-    a = 0
-    b = 1
-    while n > 1:
-        n -= 1
-        c = a + b
-        a = b
-        b = c
-    return a
+    if n <= 1:
+        return 0
+    elif n == 2:
+        return 1
+    return getNthFib(n - 1) + getNthFib(n - 2)
