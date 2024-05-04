db32472bfc0d6a5b6b66afc3d236023bbfa6b911
Sam Lu <pbcmarsh@gmail.com>
2024-05-04 02:17:25 -0500

    Update TwoNumberSum-1.py

--- TwoNumberSum-1.py
+++ TwoNumberSum-1.py
@@ -3,7 +3,7 @@
 # Time Complexity: O(n^2) n is length of array
 # Space Complexity: O(1)
 def twoNumberSum(array, targetSum):
-    for i in range(len(array)):
+    for i in range(len(array) - 1):
         for j in range(i + 1, len(array)):
             if array[i] + array[j] == targetSum:
                 return [array[i], array[j]]
