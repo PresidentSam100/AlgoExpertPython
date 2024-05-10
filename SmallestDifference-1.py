115e2086ab960bae8690aa305475903bc992384e
Sam Lu <pbcmarsh@gmail.com>
2024-05-10 11:08:48 -0500

    Update SmallestDifference-1.py

--- SmallestDifference-1.py
+++ SmallestDifference-1.py
@@ -1,23 +1,14 @@
 # Smallest Difference (Python)
 # https://www.algoexpert.io/questions/smallest-difference
-# Time Complexity: O(n * log(n) + m * log(m)) n is the length of arrayOne, m is the length of arrayTwo
+# Time Complexity: O(n * m) n is the length of arrayOne, m is the length of arrayTwo
 # Space Complexity: O(1)
 
 def smallestDifference(arrayOne, arrayTwo):
-    arrayOne.sort()
-    arrayTwo.sort()
-    left = 0
-    right = 0
-    pair = [0, float('inf')]
-    while left < len(arrayOne) and right < len(arrayTwo):
-        diff = arrayOne[left] - arrayTwo[right]
-        if abs(diff) < abs(pair[1] - pair[0]):
-            pair[0] = arrayOne[left]
-            pair[1] = arrayTwo[right]
-        if diff < 0:
-            left += 1
-        elif diff > 0:
-            right += 1
-        else:
-            return [arrayOne[left], arrayTwo[right]]
+    diff = float('inf')
+    pair = [-1, -1]
+    for num1 in arrayOne:
+        for num2 in arrayTwo:
+            if abs(num1 - num2) < diff:
+                pair = [num1, num2]
+                diff = abs(num1 - num2)
     return pair
