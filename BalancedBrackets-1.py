322bad73a2e66103d95d7809e1b0b153d6a955f1
Sam Lu <pbcmarsh@gmail.com>
2024-05-16 10:49:39 -0500

    Create BalancedBrackets-1.py

---
+++ BalancedBrackets-1.py
@@ -0,0 +1,17 @@
+# Balanced Brackets (Python)
+# https://www.algoexpert.io/questions/balanced-brackets
+# Time Complexity: O(n) n is the length of string
+# Space Complexity: On)
+
+def balancedBrackets(string):
+    stack = []
+    for bracket in string:
+        if stack and stack[-1] == '(' and bracket == ')':
+            stack.pop()
+        elif stack and stack[-1] == '[' and bracket == ']':
+            stack.pop()
+        elif stack and stack[-1] == '{' and bracket == '}':
+            stack.pop()
+        elif bracket in "()[]{}":
+            stack.append(bracket)
+    return len(stack) == 0
