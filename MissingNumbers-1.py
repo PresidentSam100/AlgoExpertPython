7beab8422552adc6b177b812217896ebd97b9cdd
Sam Lu <pbcmarsh@gmail.com>
2024-05-17 13:22:37 -0500

    Create MissingNumbers-1.py

---
+++ MissingNumbers-1.py
@@ -0,0 +1,12 @@
+# Missing Numbers (Python)
+# https://www.algoexpert.io/questions/missingNumbers
+# Time Complexity: O(n) n is number of nums
+# Space Complexity: O(n)
+
+def missingNumbers(nums):
+    setNums = set(nums)
+    missing = []
+    for num in range(1, len(nums) + 3):
+        if num not in setNums:
+            missing.append(num)
+    return missing
